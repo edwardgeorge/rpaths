8967819ac5ce280aa593d3b1356976331329c0f2
Edward George <edwardgeorge@gmail.com>
2019-11-02 12:06:53 +0100

    chore: rustfmt src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,33 @@
-use std::{io};
 use std::fs::{read_dir, read_link};
+use std::io;
 use std::path::{Path, PathBuf};
-use std::vec::{Vec};
+use std::vec::Vec;
 
 use expanduser::expanduser;
 
 fn combine_paths(inp: &Vec<PathBuf>, sep: &str) -> String {
-    let x: Vec<String> = inp.iter().map(|x| x.to_string_lossy().into_owned()).collect();
+    let x: Vec<String> = inp
+        .iter()
+        .map(|x| x.to_string_lossy().into_owned())
+        .collect();
     x[..].join(sep)
 }
 
 fn dir_links(dir: &Path) -> Vec<PathBuf> {
     let path = Path::new(dir);
-    read_dir(path).and_then(|entries| {
-        let mut paths = Vec::new();
-        for entry in entries {
-            let entry = entry?;
-            match read_link(entry.path()).and_then(|x| path.join(x).canonicalize()) {
-                Ok(p) => paths.push(p),
-                Err(_) => (),
+    read_dir(path)
+        .and_then(|entries| {
+            let mut paths = Vec::new();
+            for entry in entries {
+                let entry = entry?;
+                match read_link(entry.path()).and_then(|x| path.join(x).canonicalize()) {
+                    Ok(p) => paths.push(p),
+                    Err(_) => (),
+                }
             }
-        };
-        Ok(paths)
-    }).unwrap_or_else(|_| vec![])
+            Ok(paths)
+        })
+        .unwrap_or_else(|_| vec![])
 }
 
 fn main() -> io::Result<()> {
