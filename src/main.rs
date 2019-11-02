f6121352e096a3b56b70e4ad54c590d9a78c51d8
Edward George <edwardgeorge@gmail.com>
2019-11-02 11:58:22 +0100

    first working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io};
+use std::fs::{read_dir, read_link};
+use std::path::{Path, PathBuf};
+use std::vec::{Vec};
+
+use expanduser::expanduser;
+
+fn combine_paths(inp: &Vec<PathBuf>, sep: &str) -> String {
+    let x: Vec<String> = inp.iter().map(|x| x.to_string_lossy().into_owned()).collect();
+    x[..].join(sep)
+}
+
+fn dir_links(dir: &Path) -> Vec<PathBuf> {
+    let path = Path::new(dir);
+    read_dir(path).and_then(|entries| {
+        let mut paths = Vec::new();
+        for entry in entries {
+            let entry = entry?;
+            match read_link(entry.path()).and_then(|x| path.join(x).canonicalize()) {
+                Ok(p) => paths.push(p),
+                Err(_) => (),
+            }
+        };
+        Ok(paths)
+    }).unwrap_or_else(|_| vec![])
+}
+
+fn main() -> io::Result<()> {
+    let path = expanduser("~/.paths.d")?;
+    let paths = dir_links(path.as_path());
+    let res = combine_paths(&paths, ":");
+    println!("{}", res);
+    Ok(())
 }
